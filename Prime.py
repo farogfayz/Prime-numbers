b0834fcf6dec43ccac403f06911ad5e4fe4d735d
farog basher <58550403+farogfayz@users.noreply.github.com>
2021-06-01 17:25:59 +0300

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,6 +1,14 @@
-import math
+num = int(input("please give a number ->"))
+count=0
+number=[]
+if num>1:
+    for i in range(2,num):
+        if num%i==0:
+            number.append(i)
             count +=1
-
+        else:
+            count +=1
+            
     if len(number)>0:
         print(num,"is a composite number and the factors are ->",number)
         print("with first method number of iterations is :", count)
